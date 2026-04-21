18f23ea1183cc5ebc52eab6becfde5f78e74e1b3
Tahmid Ashfaque <ashfaquet874@gmail.com>
2026-04-21 20:17:01 +0600

    fix: refactor proxy.ts for compatibility and secret management

---
+++ src/proxy.ts
@@ -0,0 +1,17 @@
+import { NextResponse, type NextRequest } from 'next/server'
+
+export default async function proxy() {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, icon.svg, apple-icon.svg (static assets)
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
